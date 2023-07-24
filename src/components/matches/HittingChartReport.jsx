b6176b00f884a581a230a1633c9aa43668dccc58
peranasports <48541736+peranasports@users.noreply.github.com>
2023-07-24 19:57:58 +0700

    Fixed Hitting Chart filters bugs

--- src/components/matches/HittingChartReport.jsx
+++ src/components/matches/HittingChartReport.jsx
@@ -1,286 +1,308 @@
-import { useState, useEffect } from 'react'
-import { zoneFromString } from '../utils/Utils'
-import AllFiltersPanel from './AllFiltersPanel'
-import HittingChart from './HittingChart';
-import AttackZoneChart from './AttackZoneChart';
+import { useState, useEffect } from "react";
+import { zoneFromString } from "../utils/Utils";
+import AllFiltersPanel from "./AllFiltersPanel";
+import HittingChart from "./HittingChart";
+import AttackZoneChart from "./AttackZoneChart";
 import { allFilters } from "./AllFilters";
-
-const kSkillSpike = 4
+import { kSkillSpike, kSkillSet, kSkillSettersCall } from "../utils/StatsItem";
 
 function HittingChartReport({ match, selectedGame, selectedTeam }) {
-    const [drawMode, setDrawMode] = useState(0)
-    const [events, setEvents] = useState(null)
-    const [allOptions, setAllOptions] = useState(allFilters)
-    const [, forceUpdate] = useState(0);
+  const [drawMode, setDrawMode] = useState(0);
+  const [allEvents, setAllEvents] = useState(null);
+  const [events, setEvents] = useState(null);
+  const [allOptions, setAllOptions] = useState(allFilters);
+  const [init, setInit] = useState(false);
+  const [, forceUpdate] = useState(0);
 
-    const doUpdate = () => {
-        forceUpdate(n => !n)
-        setDrawMode(checkFilter('Display', 'Cone') ? 1 : 0)
-        var xevents = calculateZones()
+  const doUpdate = () => {
+    forceUpdate((n) => !n);
+    setDrawMode(checkFilter("Display", "Cone") ? 1 : 0);
+    var xevents = calculateZones();
+  };
+
+  const getAttackComboOfEvent = (code) => {
+    var acs = match.attackCombos.filter((ac) => {
+      return ac.code === code;
+    });
+    if (acs.length > 0) {
+      return acs[0];
     }
+    return null;
+  };
 
-    const getAttackComboOfEvent = (code) => {
-        var acs = match.attackCombos.filter(ac => { return ac.code === code })
-        if (acs.length > 0) {
-            return acs[0]
+  const checkFilter = (filtername, objectname) => {
+    for (var n = 0; n < allOptions.length; n++) {
+      var option = allOptions[n];
+      if (option.title === filtername) {
+        if (option.items === undefined) {
+          return true;
         }
-        return null
+        for (var ni = 0; ni < option.items.length; ni++) {
+          if (option.items[ni].name === objectname || option.items[ni].number === objectname) {
+            return option.items[ni].selected;
+          }
+        }
+      }
+    }
+    return false;
+  };
+
+  const calculateZones = () => {
+    var xevents = [
+      [[], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], []],
+    ];
+
+    var team = selectedTeam === 0 ? match.teamA : match.teamB;
+    var xevs =
+      selectedGame === 0 ? match.events : match.sets[selectedGame - 1].events;
+    var setter = null;
+    var evs = [];
+    for (var ne = 0; ne < xevs.length; ne++) {
+      var e = xevs[ne];
+      if (
+        e.Player !== null &&
+        team.players.filter((obj) => obj.Guid === e.Player.Guid).length > 0
+      ) {
+        if (e.EventType == kSkillSpike) {
+          evs.push(e);
+        }
+      }
     }
 
-    const checkFilter = (filtername, objectname) =>
+    if (init === false)
     {
-        for (var n = 0; n < allOptions.length; n++) {
-            var option = allOptions[n]
-            if (option.title === filtername) {
-                if (option.items === undefined)
-                {
-                    return true
-                }
-                for (var ni=0; ni<option.items.length; ni++)
-                {
-                    if (option.items[ni].name === objectname)
-                    {
-                        return option.items[ni].selected
-                    }
-                }
-            }
+        setAllEvents(evs);
+        setSpikerNames(evs);
+        if (match.app !== "VBStats") {
+          setSetterNames(evs);
+          setAttackCombos(evs);
         }
-        return false
+        setInit(true);
     }
 
-    const calculateZones = () => {
-        var xevents = [
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-        ]
+    const egs = ["Errors", "In-play", "In-play", "Kill"]
+    const srcs = { "F": "Front", "B": "Back", "C": "Centre" }
 
-        var team = selectedTeam === 0 ? match.teamA : match.teamB
-        var xevs = selectedGame === 0 ? match.events : match.sets[selectedGame - 1].events
-        var evs = []
-        for (var ne = 0; ne < xevs.length; ne++) {
-            var e = xevs[ne];
-            if (e.Player !== null && team.players.filter(obj => obj.Guid === e.Player.Guid).length > 0) {
-                if (e.EventType == kSkillSpike) {
-                    evs.push(e)
-                }
-            }
+    if (match.app === "VBStats") {
+      for (var ne = 0; ne < evs.length; ne++) {
+        var e = evs[ne];
+        var startZone = zoneFromString(e.BallStartString);
+        if (startZone > 0 && e.Row !== undefined && isNaN(e.Row) === false) {
+          xevents[e.Row - 1][startZone - 1].push(e);
         }
+      }
+    } else {
+      for (var ne = 0; ne < evs.length; ne++) {
+        var e = evs[ne];
+        if (checkFilter("Attackers", e.Player.LastName) === false) {
+          continue;
+        }
+        if (
+          e.setter !== null &&
+          checkFilter("Setters", e.setter) === false
+        ) {
+          continue;
+        }
+        if (checkFilter("Results", egs[e.EventGrade]) === false) {
+            continue;
+        }
+  
 
-        if (match.app === 'VBStats')
+        var nacs = match.attackCombos.length;
+        var ac = getAttackComboOfEvent(e.attackCombo);
+        if (ac === null || ac.code === undefined) {
+          continue;
+        }
+        if (checkFilter("Attack Combos", ac.code) === false) {
+          continue;
+        }
+        if (checkFilter("Source", srcs[ac.targetHitter]) === false)
         {
-            for (var ne = 0; ne < evs.length; ne++) {
-                var e = evs[ne]
-                var startZone = zoneFromString(e.BallStartString)
-                if (startZone > 0 && e.Row !== undefined && isNaN(e.Row) === false) {
-                    xevents[e.Row - 1][startZone - 1].push(e);
-                }
-            }
+            continue;
         }
-        else
-        {
-            for (var ne = 0; ne < evs.length; ne++) {
-                var e = evs[ne]
-                var nacs = match.attackCombos.length;
-                var ac = getAttackComboOfEvent(e.attackCombo)
-                if (ac === null || ac.code === undefined)
-                {
-                    continue
-                }
-                if (checkFilter("Attack Combos", ac.code) === false) {
-                    continue
-                }
-                if ((nacs > 0 && ac != null && ac.targetHitter !== "-") ||
-                    (nacs == 0)) {
-                    var row = e.Row - 1;
-                    var startZone = 0;
-                    if (nacs > 0) {
-                        if (ac.isBackcourt) {
-                            if (ac.targetHitter === "B") {
-                                startZone = 9;
-                            }
-                            else if (ac.targetHitter === "F") {
-                                startZone = 7;
-                            }
-                            else {
-                                startZone = 8;
-                            }
-                        }
-                        else {
-                            if (ac.targetHitter === "B") {
-                                startZone = 2;
-                            }
-                            else if (ac.targetHitter === "F") {
-                                startZone = 4;
-                            }
-                            else {
-                                startZone = 3;
-                            }
-                        }
-                    }
-                    else {
-                        startZone = zoneFromString(e.BallStartString);
-                    }
-                    if (startZone > 0) {
-                        xevents[row][startZone - 1].push(e);
-                    }
-                    else {
-                        // DLog(@"%@ %@", e.attackCombo, e.Player.LastName);
-                    }
-                }
-                else {
-                    // DLog(@"%@ %@", e.attackCombo, e.Player.LastName);
-                }
+        if ((nacs > 0 && ac != null && ac.targetHitter !== "-") || nacs == 0) {
+          var row = e.Row - 1;
+          var startZone = 0;
+          if (nacs > 0) {
+            if (ac.isBackcourt) {
+              if (ac.targetHitter === "B") {
+                startZone = 9;
+              } else if (ac.targetHitter === "F") {
+                startZone = 7;
+              } else {
+                startZone = 8;
+              }
+            } else {
+              if (ac.targetHitter === "B") {
+                startZone = 2;
+              } else if (ac.targetHitter === "F") {
+                startZone = 4;
+              } else {
+                startZone = 3;
+              }
             }
-            }
-        setEvents(xevents)
-        return xevents
+          } else {
+            startZone = zoneFromString(e.BallStartString);
+          }
+          if (startZone > 0) {
+            xevents[row][startZone - 1].push(e);
+          } else {
+            // DLog(@"%@ %@", e.attackCombo, e.Player.LastName);
+          }
+        } else {
+          // DLog(@"%@ %@", e.attackCombo, e.Player.LastName);
+        }
+      }
     }
+    setEvents(xevents);
+    return xevents;
+  };
 
-    const setSpikerNames = (xevents) => {
-        if (match === null) {
-            return
+  const setSpikerNames = (xevents) => {
+    if (match === null) {
+      return;
+    }
+    for (var n = 0; n < allOptions.length; n++) {
+      var option = allOptions[n];
+      if (option.title === "Attackers") {
+        option.items = [];
+        for (var ne = 0; ne < xevents.length; ne++) {
+          var evx = xevents[ne];
+          var pl = evx.Player;
+          if (
+            option.items.filter((obj) => obj.name == pl.NickName).length === 0
+          ) {
+            option.items.push({
+              name: pl.NickName,
+              selected: true,
+              amount: 0,
+            });
+          }
         }
-        for (var n = 0; n < allOptions.length; n++) {
-            var option = allOptions[n]
-            if (option.title === "Attackers") {
-                option.items = []
-                for (var ne = 0; ne < xevents.length; ne++) {
-                    var evs = xevents[ne]
-                    for (var nx = 0; nx < evs.length; nx++) {
-                        var evx = evs[nx]
-                        for (var nz = 0; nz < evx.length; nz++) {
-                            var pl = evx[nz].Player
-                            if (option.items.filter(obj => obj.name == pl.NickName).length === 0) {
-                                option.items.push({ name: pl.NickName, selected: true, amount: 0 })
-                            }
-                        }
-                    }
-                }
+      }
+    }
+  };
+
+  const setSetterNames = (xevents) => {
+    if (match === null) {
+      return;
+    }
+    for (var n = 0; n < allOptions.length; n++) {
+      var option = allOptions[n];
+      if (option.title === "Setters") {
+        var setters = [];
+        for (var ne = 0; ne < xevents.length; ne++) {
+          var evx = xevents[ne];
+          var sn = evx.setter;
+          if (setters.filter((obj) => obj == sn).length === 0) {
+            setters.push(sn);
+          }
+        }
+        option.items = [];
+        for (var nn = 0; nn < setters.length; nn++) {
+          var pls =
+            selectedTeam === 0 ? match.teamA.players : match.teamB.players;
+          for (var np = 0; np < pls.length; np++) {
+            var pl = pls[np];
+            if (pl.shirtNumber === setters[nn]) {
+              option.items.push({
+                name: pl.NickName,
+                number: pl.shirtNumber,
+                selected: true,
+                amount: 0,
+              });
+              break;
             }
+          }
         }
+      }
     }
+  };
 
-    const setSetterNames = (xevents) => {
-        if (match === null) {
-            return
-        }
-        for (var n = 0; n < allOptions.length; n++) {
-            var option = allOptions[n]
-            if (option.title === "Setters") {
-                var setters = []
-                for (var ne = 0; ne < xevents.length; ne++) {
-                    var evs = xevents[ne]
-                    for (var nx = 0; nx < evs.length; nx++) {
-                        var evx = evs[nx]
-                        for (var nz = 0; nz < evx.length; nz++) {
-                            var sn = evx[nz].setter
-                            if (setters.filter(obj => obj == sn).length === 0) {
-                                setters.push(sn)
-                            }
-                        }
-
-                    }
-                }
-                option.items = []
-                for (var nn = 0; nn < setters.length; nn++) {
-                    var pls = selectedTeam === 0 ? match.teamA.players : match.teamB.players
-                    for (var np = 0; np < pls.length; np++) {
-                        var pl = pls[np]
-                        if (pl.shirtNumber === setters[nn]) {
-                            option.items.push({ name: pl.NickName, selected: true, amount: 0 })
-                            break
-                        }
-                    }
-                }
-            }
-        }
+  const setAttackCombos = (xevents) => {
+    if (match === null) {
+      return;
     }
-
-    const setAttackCombos = (xevents) => {
-        if (match === null) {
-            return
+    for (var n = 0; n < allOptions.length; n++) {
+      var option = allOptions[n];
+      if (option.title === "Attack Combos") {
+        option.items = [];
+        for (var ne = 0; ne < xevents.length; ne++) {
+          var evx = xevents[ne];
+          var ac = evx.attackCombo;
+          if (option.items.filter((obj) => obj.name == ac).length === 0) {
+            option.items.push({ name: ac, selected: true, amount: 0 });
+          }
         }
-        for (var n = 0; n < allOptions.length; n++) {
-            var option = allOptions[n]
-            if (option.title === "Attack Combos") {
-                option.items = []
-                for (var ne = 0; ne < xevents.length; ne++) {
-                    var evs = xevents[ne]
-                    for (var nx = 0; nx < evs.length; nx++) {
-                        var evx = evs[nx]
-                        for (var nz = 0; nz < evx.length; nz++) {
-                            var ac = evx[nz].attackCombo
-                            if (option.items.filter(obj => obj.name == ac).length === 0) {
-                                option.items.push({ name: ac, selected: true, amount: 0 })
-                            }
-                        }
-                    }
-                }
-            }
-        }
+      }
     }
+  };
 
-    const selectedRows = () =>
-    {
-        var rows = []
-        for (var n = 0; n < allOptions.length; n++) {
-            var option = allOptions[n]
-            if (option.title === "Rotations") {
-                for (var ni=0; ni<option.items.length; ni++)
-                {
-                    if (option.items[ni].selected)
-                    {
-                        rows.push(option.items[ni].name)
-                    }
-                }
-            }
+  const selectedRows = () => {
+    var rows = [];
+    for (var n = 0; n < allOptions.length; n++) {
+      var option = allOptions[n];
+      if (option.title === "Rotations") {
+        for (var ni = 0; ni < option.items.length; ni++) {
+          if (option.items[ni].selected) {
+            rows.push(option.items[ni].name);
+          }
         }
-        return rows
+      }
     }
+    return rows;
+  };
 
-    useEffect(() => {
-        var xevents = calculateZones()
-        setSpikerNames(xevents)
-        if (match.app !== 'VBStats')
-        {
-            setSetterNames(xevents)
-            setAttackCombos(xevents)
-        }
-        forceUpdate(n => !n)
-    }, [selectedGame, selectedTeam])
+  useEffect(() => {
+    var xevents = calculateZones();
+    // setSpikerNames(allEvents);
+    // if (match.app !== "VBStats") {
+    //   setSetterNames(allEvents);
+    //   setAttackCombos(allEvents);
+    // }
+    forceUpdate((n) => !n);
+  }, [selectedGame, selectedTeam]);
 
-    if (events === null) {
-        return <></>
-    }
+  useEffect(() => {}, [events]);
 
-    return (
-        <div>
-            <div className="drawer drawer-mobile">
-                <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content">
-                    <div className="w-100 h-full">
-                        <HittingChart match={match} events={events} rows={selectedRows()} drawMode={drawMode} />
-                    </div>
-                </div>
-                <div className="drawer-side w-80">
-                    <label htmlFor="my-drawer-5" className="drawer-overlay"></label>
-                    <div className="h-full bg-base-200">
-                        <AllFiltersPanel
-                            allOptions={allOptions}
-                            match={match}
-                            events={events}
-                            selectedTeam={selectedTeam}
-                            handleFilterOptionChanged={() => doUpdate()}
-                        />
-                    </div>
-                </div>
-            </div>
+  if (events === null) {
+    return <></>;
+  }
+
+  return (
+    <div>
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <div className="w-100 h-full">
+            <HittingChart
+              match={match}
+              events={events}
+              rows={selectedRows()}
+              drawMode={drawMode}
+            />
+          </div>
         </div>
-    )
+        <div className="drawer-side w-80">
+          <label htmlFor="my-drawer-5" className="drawer-overlay"></label>
+          <div className="h-full bg-base-200">
+            <AllFiltersPanel
+              allOptions={allOptions}
+              match={match}
+              events={events}
+              selectedTeam={selectedTeam}
+              handleFilterOptionChanged={() => doUpdate()}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HittingChartReport
+export default HittingChartReport;
